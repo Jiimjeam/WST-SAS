e34b3a68ae265ccda21d788912073d766e13e9e2
jeamjim <limj1674@gmail.com>
2025-04-18 22:20:17 +0800

    feature toggle

--- app/Models/FeatureSetting.php
+++ app/Models/FeatureSetting.php
@@ -3,8 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use OwenIt\Auditing\Contracts\Auditable;
 
-class FeatureSetting extends Model
+class FeatureSetting extends Model implements Auditable
 {
+    
+    use \OwenIt\Auditing\Auditable;
+
     protected $fillable = ['feature_name', 'is_enabled'];
 }
