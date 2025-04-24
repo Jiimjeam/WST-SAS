858cec7e100806bdd7e241a3d998825018157e48
jeamjim <limj1674@gmail.com>
2025-04-25 07:22:20 +0800

    ui custom

--- database/migrations/tenant/2025_04_06_000713_create_users_table.php
+++ database/migrations/tenant/2025_04_06_000713_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->string('position');
             $table->string('password');
+            $table->string('sidebar_color')->nullable()->default('#047857'); // default green
             $table->timestamps();
         });
     }
